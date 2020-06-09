72d3b9ba02886a705a999f3a17aa70d98944f44d
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-06-09 09:54:04 +0200

    Add LICENSE.txt, README.md and gulpfile.js

---
+++ README.md
@@ -0,0 +1,19 @@
+Embeddable GraphDebug for the Web
+=================================
+
+This repository contains a GraphDebug implementation that can be embedded into
+IDEs that support rendering web content, such as [Atom](https://atom.io/) and
+[Visual Studio Code](https://code.visualstudio.com/).
+
+## Usage
+
+There is currently no release yet, so you will have to wait for the first
+version to be available.
+
+## License
+
+This project is licensed under the GNU Public License, version 3. Projects may
+use this software to debug their own applications, but may not sell this
+software under their own name. Please contact us if you require a less
+restrictive license.
+
---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+
+const gulp = require('gulp')
+const webpack = require('webpack-stream');
+const merge = require('webpack-merge');
+const minimist = require('minimist');
+const log = require('fancy-log');
+
+// Files that will be copied to dist/ without any modifications
+const AUX_FILES = ['package.json', 'README.md', 'LICENSE.txt'];
+
+// Add all your configuration overrides for Webpack here
+const sharedWebpackConfig = {
+  mode: 'production',
+  resolve: { extensions: ['.js', '.ts'] },
+  module: {
+    rules: [
+      { test: /\.ts$/, exclude: /node_modules/, loader: "ts-loader", options: { transpileOnly: true } },
+      { test: /\.m?js$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ]
+  }
+}
+
+const args = minimist(process.argv.slice(2));
+
+if (args.dev) {
+  log.info(`Building in development mode.`);
+  sharedWebpackConfig.mode = 'development';
+  sharedWebpackConfig.devtool = 'source-map';
+}
+
+function copyAux() {
+  return gulp.src(AUX_FILES)
+    .pipe(gulp.dest('dist/'));
+}
+
+function bundle() {
+  return gulp.src(['./src/index.ts', './src/script.ts'])
+    .pipe(webpack({
+      config: [
+        merge(sharedWebpackConfig, {
+          entry: './src/index.ts',
+          target: 'node',
+          output: { filename: 'index.js' },
+        }),
+        merge(sharedWebpackConfig, {
+          entry: './src/script.ts',
+          target: 'web',
+          output: { filename: 'script.js' },
+        })
+      ]
+    }))
+    .pipe(gulp.dest('dist/'));
+}
+
+const build = gulp.parallel(bundle, copyAux);
+
+function startWatch() {
+  return gulp.watch(
+    ['src/**/*.ts', 'package.json', 'README.md', 'LICENSE.txt'],
+    build,
+  )
+}
+
+const watch = gulp.series(build, startWatch)
+
+module.exports = {
+  build,
+  watch,
+}
+
