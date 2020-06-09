4a7e43afdaa2bc2963a4ff483b13bfd692f6782f
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-06-09 20:10:51 +0200

    Temporarily add a test snippet to the repo

---
+++ test.js
@@ -0,0 +1,18 @@
+
+class Point {
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+}
+
+const p1 = new Point(10, 15);
+const p2 = new Point(1, 4);
+
+// You can set a breakpoint on this line
+debugger;
+
+console.log('BLA');
+
